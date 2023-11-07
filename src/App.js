b7beddf5cb1fdb4a6353bd4800e84f289c300466
Abishake01 <abishake381@gmail.com>
2023-11-07 15:51:42 +0530

    recent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import img1 from './image/avenger.jpg'
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name,setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [fathername, setFatherName] = useState('');
+  const [detail, setDetail] = useState([]);
+
+  const onsubmit = (e) => {
+    console.log(name,fathername,location);
+    e.preventDefault();
+    const newDetail = {
+      name,location,fathername
+    }
+    console.log(newDetail);
+    setDetail([...detail, newDetail]);
+    console.log(detail);
+  }
+
+  const deleteDetail = (index) => {
+    const update = detail.filter((_,i) => i !== index);
+    setDetail(update);
+  }
+  
   return (
-    <div className="App">
-      <img src={img1} alt="image" />
+    <div className=''>
+      <form className='flex flex-col gap-4'>
+        <input type='text' onChange={(e) => setName(e.target.value)} placeholder='Enter your name' required></input>
+        <input type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Enter your location' required></input>
+        <input type='text' onChange={(e) => setFatherName(e.target.value)} placeholder='Enter your father name' required></input>
+        <input type='submit' onClick={onsubmit} value={'Submit'} />
+      </form>
+      <div>
+        {detail.map((info, index) => (
+          <div key={index}>
+              <h1>Detail</h1>
+              <p>Name: {info.name}</p>
+              <p>Location: {info.location}</p>
+              <p>Father Name: {info.fathername}</p>
+              <button onClick={() => deleteDetail(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
